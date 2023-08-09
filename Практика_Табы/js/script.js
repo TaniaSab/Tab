cd511b526c92b60211f45d5dd09400ab980e713d
TaniaSab <140402968+TaniaSab@users.noreply.github.com>
2023-08-09 18:16:00 +0300

    Update script.js

--- Практика_Табы/js/script.js
+++ Практика_Табы/js/script.js
@@ -1,34 +1,41 @@
 const one_tabs = document.querySelectorAll(".one-tabs__text"); 
 const main__button_gray  = document.querySelectorAll(".main__button_gray");
+const one_info = document.querySelectorAll(".main__grid_two-tabs-one");
 
+
+var b= 0;
 var a = 0;
+
+
+
 one_tabs.forEach(function(item){
     item.addEventListener("click", function()
-    {
-     one_tabs.forEach(function(item){
-        item.classList.remove("active")
-      })
-    item.classList.add("active");
-    item.classList.add("click");
+    {    
+        one_tabs.forEach(function(item)
+            {
+                item.classList.remove("active")
+                b +=1;
+            })
+        item.classList.add("active");
+        item.classList.add("click");
+        var className = item.className;
 
-    var className = item.className;
 
+        if (className.indexOf("active click") != -1)
+            {
+                a +=1;
+            }
+    
 
-    if (className.indexOf("active click") != -1)
-    {
-        a = a+1
-    }
-   
-
-    if (a == 4) 
-    {
-        main__button_gray.forEach(function(item)
-        {
-            item.classList.remove("disabled");
-        })
-    }
+        if (a == b) 
+            {
+                main__button_gray.forEach(function(item)
+                {
+                    item.classList.remove("disabled");
+                })
+            }
+            b=0;
    })
+   
 });
-
-
 
